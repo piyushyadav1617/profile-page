26cdc7828446c0da8ee6e947fbac08048989dee9
Piyush Yadav <72244570+piyushyadav1617@users.noreply.github.com>
2023-05-13 22:51:59 +0530

    Update route.js

--- server/router/route.js
+++ server/router/route.js
@@ -4,21 +4,21 @@ const router = Router();
 
 
 /** POST Methods */
-router.route('/register').post(controller.register); // register user
-// router.route('/registerMail').post(); // send the email
-router.route('/authenticate').post((req, res) => res.end()); // authenticate user
-router.route('/login').post(controller.login); // login in app
+router.route('/register').post(controller.register); 
+
+router.route('/authenticate').post((req, res) => res.end()); 
+router.route('/login').post(controller.login);
 
 /** GET Methods */
-router.route('/user/:username').get(controller.getUser) // user with username
-router.route('/generateOTP').get(controller.generateOTP) // generate random OTP
-router.route('/verifyOTP').get(controller.verifyOTP) // verify generated OTP
+router.route('/user/:username').get(controller.getUser) 
+router.route('/generateOTP').get(controller.generateOTP) 
+router.route('/verifyOTP').get(controller.verifyOTP) // 
 router.route('/createResetSession').get(controller.createResetSession) // reset all the variables
 
 
 /** PUT Methods */
-router.route('/updateuser').put(controller.updateUser); // is use to update the user profile
-router.route('/resetPassword').put(controller.resetPassword); // use to reset password
+router.route('/updateuser').put(controller.updateUser); 
+router.route('/resetPassword').put(controller.resetPassword); 
 
 
 
